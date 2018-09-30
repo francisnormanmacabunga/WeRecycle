6d390b1d5a86a4af5d1f8dff5fc219df528a57a6
francisnormanmacabunga <francisnormanmacabunga@gmail.com>
2018-09-30 19:56:04 +0800

    Volunteer Count!

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,8 +5,31 @@
     <h3>Welcome to landing page!</h3>
     <p class="lead">This is WeRecycle!!</p>
 
+    <div class="row">
+          <div class="container col-md-4 center-align">
+        <h4 class="d-flex justify-content-between align-items-center mb-3">
+
+        </h4>
+        <ul class="list-group mb-3">
+
+
+          <li class="list-group-item d-flex justify-content-between">
+            <span>Volunteers attending</span>
+            <strong>{{ $volunteersCount->count() }}</strong>
+          </li>
+          <li class="list-group-item d-flex justify-content-between">
+            <span>Volunteers needed</span>
+            <strong>10</strong>
+          </li>
+        </ul>
+
+
+      </div>
+    </div>
 
     <a class="btn btn-lg btn-primary" href="/createApplicant" role="button">Apply as a Volunteer </a>
+
+
   </div>
 
   <div class="jumbotron mt-3 text-center">

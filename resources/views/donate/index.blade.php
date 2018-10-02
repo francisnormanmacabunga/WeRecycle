639dad53978284c5b483ecbfb71c5b6f7af8a1ff
cvreniel <cvreniel@gmail.com>
2018-10-03 00:00:34 +0800

    fix

--- resources/views/donate/index.blade.php
+++ resources/views/donate/index.blade.php
@@ -2,19 +2,7 @@
 @section('content')
 <div class="container">
     <div class="content">
-
-      @if(session()->has('notif'))
-      <div class="content">
-        <div class="alert alert-success">
-          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-          <strong>{{session()->get('notif')}}</strong>
-        </div>
-      </div>
-      @endif
-
         <h3>Donation list</h3>
-
-
         <table class="table table-hover">
             <thead>
             <tr>

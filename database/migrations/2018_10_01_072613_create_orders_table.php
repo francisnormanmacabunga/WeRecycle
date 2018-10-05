f190f9598e465fb9a46e252af8580cb748e651fc
francisnormanmacabunga <francisnormanmacabunga@gmail.com>
2018-10-05 22:58:51 +0800

    Changes

--- database/migrations/2018_10_01_072613_create_orders_table.php
+++ database/migrations/2018_10_01_072613_create_orders_table.php
@@ -14,29 +14,17 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->increments('id');
-=======
             $table->increments('orderid');
->>>>>>> 8cf2bed61f241bd5d927d7e419ae00e230ff033e
             $table->integer('userID');
             $table->text('cart');
             $table->string('fname');
             $table->string('lname');
-<<<<<<< HEAD
-            $table->string('username');
-            $table->string('email');
-=======
->>>>>>> 8cf2bed61f241bd5d927d7e419ae00e230ff033e
             $table->text('street');
             $table->integer('barangay');
             $table->string('city');
             $table->integer('zip');
-<<<<<<< HEAD
-=======
             $table->string('status');
             $table->integer('contactno');
->>>>>>> 8cf2bed61f241bd5d927d7e419ae00e230ff033e
             $table->timestamps();
         });
     }

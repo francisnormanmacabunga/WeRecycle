97fa5df881574c23517f4c1df1114a3361041797
francisnormanmacabunga <francisnormanmacabunga@gmail.com>
2018-10-05 16:44:43 +0800

    Fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -34,52 +34,7 @@ class PagesController extends Controller
       ->orderBy('created_at', 'desc')
       ->get();
 
-<<<<<<< HEAD
       //dd($audits);
-=======
-      $audits = userTable::first();
-      //$all = audits()->with('userTable')->get();
-      //$userTable = userTable;
-      //$all = $userTable->audits;
-      //$audits = $donors->audits()->with('user')->get();
-      //$audits = userTable::find(1);
-      //$audits = userTable::with('audits.userTable'->find($model->getKey());
-      return view('audits.index', compact('audits'));
-    }
-
-    public function index2(){
-      $volunteersCount = userTable::SELECT('*')
-      ->where('status', 'Activated')
-      ->where('usertypeID', '2')
-      ->get();
-
-      return view('pages.index2', compact('volunteersCount'));
-    }
-
-    //public function createEmployee(){
-      //return view('employees.create');
-    //}
-
-    //public function createCatalog(){
-      //return view('catalog.create');
-    //}
-
-    //public function createFeedback(){
-      //return view('usersFeedback.create');
-    //}
-
-    //public function login(){
-      //return view('login.login');
-    //}
-
-    //public function indexUser(){
-        //return view('pages.indexUser');
-    //}
-
-    //public function indexAC(){
-      //return view('pages.indexAC');
-    //}
->>>>>>> 4f8730ee4ca7d81b669bd5cb1fe2fe8a7f9c5089
 
       return view('audits.index', ['audits' => $audits]);
 

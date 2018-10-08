baef9029568ada595ed0a18112a39308cf156af6
jamespramono <jameskylejoko.pramono@benilde.edu.ph>
2018-10-08 16:11:57 +0800

    asd

--- resources/views/Donor/History/donationHistory.blade.php
+++ resources/views/Donor/History/donationHistory.blade.php
@@ -1,45 +1,31 @@
 @extends('layouts.frontend')
-@include('layouts.pd-nav')
+@include('layouts.donor-nav')
 
 @section('content')
 
 <div>
-<div class="row">
+    <div class="row">
 
-      <h1>Donation History</h1>
-
-      <table class="table table-bordered">
-      <tr>
-      <th>Username</th>
-      <th>Type of donation</th>
-      <th>Order(name,price,quantity)</th>
-      <th>Assigned Volunteer</th>
-      <th>Date</th>
-      <th>Status</th>
-    </tr>
-
-    <!--@foreach($cartItems as $item)
-    <tr>
-<<<<<<< HEAD:resources/views/program_directors/donationHistory.blade.php
-<?php  ?>
-      <td>{{$donationhistory[0]['fname']}}</td>
-=======
-
-      <td>{{$donationhistory['fname']}}</td>
->>>>>>> 266e8d37e45fba61e019f3d07cf512f3efe6884e:resources/views/Donor/History/donationHistory.blade.php
-      <td></td>
-      <td>{{$item->name}},{{$item->price}},{{$item->qty}}</td>
-      <td></td>
-      <td>{{$donationhistory['created_at']}}</td>
-      <td>{{$donationhistory['status']}}</td>
-    </tr>
-    @endforeach -->
-  </table>
-
-
-
-</div>
-</div>
+          <h1>Donation History</h1>
+          <table class="table table-bordered">
+        <tr>
+          <th>Type of donation</th>
+          <th>Weight</th>
+          <th>Assigned Volunteer</th>
+          <th>Date</th>
+          <th>Status</th>
+        </tr>
+        @foreach ($donationHistory as $history)
+          <tr>
+            <td>{{$history->userID}}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        @endforeach
+      </table>
+    </div>
+  </div>
 </div>
 
 @endsection

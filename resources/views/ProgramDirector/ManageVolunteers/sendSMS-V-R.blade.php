98c1516d823ca5a74431fbf7740aae14bc6b524d
francisnormanmacabunga <francisnormanmacabunga@gmail.com>
2018-10-15 15:55:32 +0800

    Commit

--- resources/views/ProgramDirector/ManageVolunteers/sendSMS-V-R.blade.php
+++ resources/views/ProgramDirector/ManageVolunteers/sendSMS-V-R.blade.php
@@ -18,7 +18,7 @@
                     @if(count($applicants)>0)
                     <table class="table table-striped table-hover">
                       <tr>
-                    
+
                         <th>Name</th>
                         <th>Email</th>
                         <th>Mobile Number</th>
@@ -28,16 +28,14 @@
                         <tr>
                           <input type= "hidden" name= "mobile" class= "radio" value="{{$applicant->cellNo}}"/>
                           <input type= "hidden" name= "userID"  value="{{$applicant->userID}}">
-                          @foreach ($transaction as $transactions)
-                           <td> <input type="number" min="0" max="9" step="2" value="{{$transactions->transid}}" > </<td>
-                          @endforeach
+                    
 
                           <td>{{$applicant->firstname}} {{$applicant->lastname}}</td>
                           <td>{{$applicant->email}}</td>
                           <td>{{$applicant->cellNo}}</td>
                           <td>
                             <a class="btn btn-primary"
-                            href="/programdirector/sendSMS-V-R/volunteerID={{$applicant->userID}}"
+                            href="/programdirector/sendSMS-V-R/volunteerID={{$applicant->volunteerID}}"
                             role="button"> Select </a>
                           </td>
                         </tr>

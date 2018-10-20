58a0631b0121cff9db9a47aafade1115c8a7367e
cvreniel <cvreniel@gmail.com>
2018-10-20 13:08:58 +0800

    asd

--- config/app.php
+++ config/app.php
@@ -151,10 +151,7 @@ return [
         Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
         Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
 
-<<<<<<< HEAD
-=======
 
->>>>>>> c0973fc5b4fc42b94c076f23f71e3cbcc7abdfb4
         /*
          * Package Service Providers...
          */
--- resources/views/pages/indexUser.blade.php
+++ resources/views/pages/indexUser.blade.php
@@ -38,7 +38,7 @@
                           </div>
                           <div class="w3-light-grey">
                             <div class="content-center"><center>your points:</center></div>
-                            <div class="w3-container w3-green"  style=" max-width:100; width:{{$width->pointsaccumulated}};%">{{$width->pointsaccumulated}}%</div>
+                            <div class="w3-container w3-green"  style=" max-width:100%; width:{{$width->pointsaccumulated}}%" max="100%" min="0%">{{$width->pointsaccumulated}}%</div>
                             <div class="content-center"><center>100% = discount code</center></div>
                           </div>
 

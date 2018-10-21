5ee13f3abebb5bd74279f8534d1d47e3c85ccb23
francisnormanmacabunga <francisnormanmacabunga@gmail.com>
2018-10-22 00:42:17 +0800

    s

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,7 +11,7 @@
     <title>Laravel</title>
 
     <!-- Scripts -->
-    <script src="http://practice.test/js/app.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -51,7 +51,7 @@
             </div>
         </nav>
 
-      
+
     </div>
 </body>
 </html>

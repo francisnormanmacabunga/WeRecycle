07443f97947220eaf18c4868441a7a3064709cdb
amielalcala <amielalcala05@gmail.com>
2018-10-23 15:16:34 +0800

    donor

--- resources/views/Admin/Donor/index.blade.php
+++ resources/views/Admin/Donor/index.blade.php
@@ -1,60 +1,78 @@
-@extends('layouts.frontend')
-@include('layouts.admin-nav')
-
-@section('content')
-
-<div class="row">
-  <div class="col-md-12">
-    <br/>
-    <h3 align="center">List of Donors</h3>
-    <a href="/admin/donors">
-     <button style="float: right;">Reset</button>
-    </a>
-    <a href="/admin/donors/?status=Deactivated">
-      <button style="float: right;">Sort by Deactivated</button>
-    </a>
-    <a href="/admin/donors/?status=Activated">
-      <button style="float: right;">Sort by Activated</button>
-    </a>
-    <br/>
-    <br/>
-      @if(count($donors) > 0)
-    <table class="table table-bordered">
-      <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Address</th>
-        <th>Barangay</th>
-        <th>Cellphone Number</th>
-        <th>Tellphone Number</th>
-        <th>@sortablelink('created_at', 'Date Created')</th>
-        <th>Status</th>
-        <th>Action</th>
-      </tr>
-      @foreach ($donors as $donor)
-        <tr>
-          <td> {{$donor->firstname}} {{$donor->lastname}}</td>
-          <td> {{$donor->age()}} </td>
-          <td> {{$donor->street}}, {{$donor->city}} </td>
-          <td> {{$donor->barangay}} </td>
-          <td> {{$donor->contacts['cellNo']}} </td>
-          <td> {{$donor->contacts['tellNo']}} </td>
-          <td> {{date('F d, Y, h:i:sa', strtotime($donor->created_at))}} </td>
-          <td> {{$donor->status}} </td>
-          <th>
-            <a class="btn btn-lg btn-block btn-primary" href="/admin/donors/{{$donor->userID}}/edit" role="button">Update Status </a>
-          </th>
-        @endforeach
-    </table>
-  @else
-  <div align="center" style="color:red;">
-    <br>
-    <br>
-    <h5 style="font-family:serif;">No records found.</h5>
-  </div>
-  @endif
-  {{$donors->links()}}
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<body>
+<div id="main-wrapper">
+  @include('navbar.admin-navbar')
+  <div class="page-wrapper">
+    <!-- ============================================================== -->
+    <!-- End Bread crumb and right sidebar toggle -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Container fluid  -->
+    <!-- ============================================================== -->
+    <div class="container-fluid">
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/donors" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Reset</span></a> </li>
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/donors/?status=Deactivated" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Deactivated</span></a> </li>
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/donors/?status=Activated" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Activated</span></a> </li>
+        </ul>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">List of Donors</h5>
+                        <div class="table-responsive">
+                          @if(count($donors) > 0)
+                            <table id="zero_config" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                      <th>Name</th>
+                                      <th>Age</th>
+                                      <th>Address</th>
+                                      <th>Barangay</th>
+                                      <th>Cellphone Number</th>
+                                      <th>Tellphone Number</th>
+                                      <th>Date Created</th>
+                                      <th>Status</th>
+                                      <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                  @foreach ($donors as $donor)
+                                    <tr>
+                                      <td> {{$donor->firstname}} {{$donor->lastname}}</td>
+                                      <td> {{$donor->age()}} </td>
+                                      <td> {{$donor->street}}, {{$donor->city}} </td>
+                                      <td> {{$donor->barangay}} </td>
+                                      <td> {{$donor->contacts['cellNo']}} </td>
+                                      <td> {{$donor->contacts['tellNo']}} </td>
+                                      <td> {{date('F d, Y, h:i:sa', strtotime($donor->created_at))}} </td>
+                                      <td> {{$donor->status}} </td>
+                                      <td>
+                                        <a href="/admin/donors/{{$donor->userID}}/edit" data-toggle="tooltip" data-placement="top" title="Edit"><i class="mdi mdi-update"></i></a>
+                                      </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            @else
+                            <div align="center" style="color:red;">
+                              <br>
+                              <br>
+                              <h5>No records found.</h5>
+                            </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer text-center">
+      Copyright &copy; 2018 WeRecycle
+    </footer>
   </div>
 </div>
-
-@endsection
+@include('navbar.footer')
+  </body>
+  </html>

e4d29e7b459a121818e93a136b451a82f3220989
amielalcala <amielalcala05@gmail.com>
2018-10-23 16:03:57 +0800

    catalog

--- resources/views/Admin/Catalog/manageShop.blade.php
+++ resources/views/Admin/Catalog/manageShop.blade.php
@@ -1,55 +1,73 @@
-@extends('layouts.frontend')
-@include('layouts.admin-nav')
-
-
-@section('content')
-
-    <div class="row">
-      <div class="col-lg-3">
-      <h4>Manage Shop Catalog</h4>
-      <div class="list-group">
-          <a href="/admin/managedonation" class="list-group-item">Manage Donation Catalog</a>
-          <a href="/admin/manageshop" class="list-group-item">Manage Shop Catalog</a>
-      </div>
-      </div>
-
-    <div class="col-md-9">
-    <div class="row">
-
-      @if(count($products2) > 0)
-          <table class="table table-bordered">
-          <tr>
-          <th>Item Type</th>
-          <th>Name</th>
-          <th>Preview</th>
-          <th>Price</th>
-          <th>Description</th>
-          <th>@sortablelink('created_at', 'Date Created')</th>
-          <th>@sortablelink('status', 'Status')</th>
-          <th>Action</th>
-        </tr>
-        @foreach ($products2 as $products)
-        <tr>
-          <td>{{$products->productstype}}</td>
-          <td>{{$products->productname}}</td>
-          <td><img src="{{ asset('images/' . $products->productimage) }}" width="200" height="200"></td>
-          <td>{{$products->price}}</td>
-          <td>{{$products->description}}</td>
-          <td>{{date('F d, Y, h:i:sa', strtotime($products->created_at))}}</td>
-          <td>{{$products->status}}</td>
-          <th><a class="btn btn-block btn-primary" href="/admin/catalog/{{$products->productsID}}/edit" role="button">Update Status </a></th>
-        </tr>
-        @endforeach
-      </table>
-    @else
-    <div align="center" style="color:red;">
-      <br>
-      <br>
-      <h5 style="font-family:serif;">No records in shop catalog found.</h5>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<body>
+<div id="main-wrapper">
+  @include('navbar.admin-navbar')
+  <div class="page-wrapper">
+    <!-- ============================================================== -->
+    <!-- End Bread crumb and right sidebar toggle -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Container fluid  -->
+    <!-- ============================================================== -->
+    <div class="container-fluid">
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="nav-item"> <a class="nav-link" href="/admin/managedonation" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Donation</span></a> </li>
+          <li class="nav-item"> <a class="nav-link active" href="/admin/manageshop" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Shop</span></a> </li>
+        </ul>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Manage Shop Catalog</h5>
+                        <div class="table-responsive">
+                          @if(count($products2) > 0)
+                            <table id="zero_config" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                      <th>Item Type</th>
+                                      <th>Name</th>
+                                      <th>Preview</th>
+                                      <th>Price</th>
+                                      <th>Description</th>
+                                      <th>Date Created</th>
+                                      <th>Status</th>
+                                      <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                  @foreach ($products2 as $products)
+                                    <tr>
+                                      <td>{{$products->productstype}}</td>
+                                      <td>{{$products->productname}}</td>
+                                      <td><img src="{{ asset('images/' . $products->productimage) }}" width="200" height="200"></td>
+                                      <td>{{$products->price}}</td>
+                                      <td>{{$products->description}}</td>
+                                      <td>{{date('F d, Y, h:i:sa', strtotime($products->created_at))}}</td>
+                                      <td>{{$products->status}}</td>
+                                      <td><a href="/admin/catalog/{{$products->productsID}}/edit" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fas fa-edit"></i></a></td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            @else
+                            <div align="center" style="color:red;">
+                              <br>
+                              <br>
+                              <h5>No records found.</h5>
+                            </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    @endif
-    {{$products2->links()}}
-    </div>
+    <footer class="footer text-center">
+      Copyright &copy; 2018 WeRecycle
+    </footer>
   </div>
 </div>
-@endsection
+@include('navbar.footer')
+  </body>
+  </html>

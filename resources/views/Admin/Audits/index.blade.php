00c5eb4a8396d0d5ff825fa25981705234b5ff4d
amielalcala <amielalcala05@gmail.com>
2018-10-24 01:20:16 +0800

    admin

--- resources/views/Admin/Audits/index.blade.php
+++ resources/views/Admin/Audits/index.blade.php
@@ -1,63 +1,72 @@
-@extends('layouts.frontend')
-@include('layouts.admin-nav')
-
-@section('content')
-
-  <div class="row">
-    <div class="col-md-12">
-      <br/>
-      <h3 align="center">Audit Logs</h3>
-      <a href="/admin/auditlogs">
-       <button style="float: right;">Reset</button>
-      </a>
-      <a href="/admin/auditlogs/?log_name=Product">
-        <button style="float: right;">Sort by Product</button>
-      </a>
-      <a href="/admin/auditlogs/?log_name=Volunteer">
-        <button style="float: right;">Sort by Volunteer</button>
-      </a>
-      <a href="/admin/auditlogs/?log_name=Program Director Account">
-        <button style="float: right;">Sort by Program Director</button>
-      </a>
-      <a href="/admin/auditlogs/?log_name=Transaction">
-        <button style="float: right;">Sort by Transaction</button>
-      </a>
-      <a href="/admin/auditlogs/?log_name=Donor Account">
-        <button style="float: right;">Sort by Donor</button>
-      </a>
-      <a href="/admin/auditlogs/?log_name=Employee Account">
-        <button style="float: right;">Sort by Employee</button>
-      </a>
-      <br/>
-      <br/>
-        @if(count($lastActivity) > 0)
-      <table class="table table-bordered">
-        <tr>
-          <th>Date</th>
-          <th>Causer</th>
-          <th>Action</th>
-          <th>Subject</th>
-          <th>Subject Type</th>
-        </tr>
-        @foreach ($lastActivity as $lastActivitys)
-        <tr>
-          <td> {{date('F d, Y, h:i:sa', strtotime($lastActivitys->updated_at))}} </td>
-          <td> {{$lastActivitys->causer['username']}} </td>
-          <td> {{$lastActivitys->description}} </td>
-          <td> {{$lastActivitys->subject['username']}} </td>
-          <td> {{$lastActivitys->log_name}} </td>
-        </tr>
-        @endforeach
-      </table>
-    @else
-    <div align="center" style="color:red;">
-      <br>
-      <br>
-      <h5 style="font-family:serif;">No records found.</h5>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<body>
+<div id="main-wrapper">
+  @include('navbar.admin-navbar')
+  <div class="page-wrapper">
+    <!-- ============================================================== -->
+    <!-- End Bread crumb and right sidebar toggle -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Container fluid  -->
+    <!-- ============================================================== -->
+    <div class="container-fluid">
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/auditlogs" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Reset</span></a> </li>
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/auditlogs/?log_name=Product" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Product</span></a> </li>
+          <!-- <li class="nav-item"> <a class="nav-link btn-outline-info" href="admin/auditlogs/?log_name=Volunteer" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Volunteer</span></a> </li> -->
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/auditlogs/?log_name=Program Director Account" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Program Director</span></a> </li>
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/auditlogs/?log_name=Transaction" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Transaction</span></a> </li>
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/auditlogs/?log_name=Donor Account" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Donor</span></a> </li>
+          <li class="nav-item"> <a class="nav-link btn-outline-info" href="/admin/auditlogs/?log_name=Employee Account" role="tab"><span class="hidden-sm-up"></span> <span class="hidden-xs-down">Employee</span></a> </li>
+        </ul>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Audit Logs</h5>
+                        <div class="table-responsive">
+                          @if(count($lastActivity) > 0)
+                            <table id="zero_config" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                      <th>Date</th>
+                                      <th>Causer</th>
+                                      <th>Action</th>
+                                      <th>Subject</th>
+                                      <th>Subject Type</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                  @foreach ($lastActivity as $lastActivitys)
+                                    <tr>
+                                      <td> {{date('F d, Y, h:i:sa', strtotime($lastActivitys->updated_at))}} </td>
+                                      <td> {{$lastActivitys->causer['username']}} </td>
+                                      <td> {{$lastActivitys->description}} </td>
+                                      <td> {{$lastActivitys->subject['username']}} </td>
+                                      <td> {{$lastActivitys->log_name}} </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            @else
+                            <div align="center" style="color:red;">
+                              <br>
+                              <br>
+                              <h5>No records found.</h5>
+                            </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    @endif
-    {{$lastActivity->links()}}
-    </div>
+    <footer class="footer text-center">
+      Copyright &copy; 2018 WeRecycle
+    </footer>
   </div>
-
-@endsection
+</div>
+@include('navbar.footer')
+  </body>
+  </html>

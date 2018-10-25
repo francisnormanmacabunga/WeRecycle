34e2e82f6062f696d0c4b8d77ada43538741374d
amielalcala <amielalcala05@gmail.com>
2018-10-26 01:44:00 +0800

    edit

--- resources/views/ProgramDirector/Profile/edit.blade.php
+++ resources/views/ProgramDirector/Profile/edit.blade.php
@@ -63,7 +63,7 @@
                                               </div>
                                               <div class="modal-footer">
                                                   <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                                                  <button type="submit" class="btn btn-success">Save changes</button>
+                                                  <button type="submit" class="btn btn-success">Save</button>
                                               </div>
                                           </div>
                                       </div>

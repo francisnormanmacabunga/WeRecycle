e67c1db002d9afec611ccc0b276f6e558bfeca3d
jamespramono <jameskylejoko.pramono@benilde.edu.ph>
2018-10-25 16:34:59 +0800

    program director

--- resources/views/navbar/pd-navbar.blade.php
+++ resources/views/navbar/pd-navbar.blade.php
@@ -50,14 +50,16 @@
                 <b class="logo-icon p-l-10">
                     <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                     <!-- Dark Logo icon -->
-                    <img src="../../../assets/images/logo-icon.png" alt="homepage" class="light-logo" />
+
+                    <img src="{{asset('assets/images/logo-icon.png')}}" alt="homepage" class="light-logo" />
 
                 </b>
                 <!--End Logo icon -->
                  <!-- Logo text -->
                 <span class="logo-text">
                      <!-- dark Logo text -->
-                     <img src="../../../assets/images/logo-text.png" alt="homepage" class="light-logo" />
+
+                     <img src="{{asset('assets/images/logo-text.png')}}" alt="homepage" class="light-logo" />
 
                 </span>
             </a>
@@ -84,7 +86,7 @@
                 <!-- User profile and search -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="../../../assets/images/users/1.jpg" alt="user" class="rounded-circle" width="31"></a>
+                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{asset('assets/images/users/1.jpg')}}" alt="user" class="rounded-circle" width="31"></a>
                     <div class="dropdown-menu dropdown-menu-right user-dd animated">
                       <a class="dropdown-item"><h5>{{Auth::user()->firstname}} {{Auth::user()->lastname}}</h5>
                         {{ Auth::user()->username }}</a>

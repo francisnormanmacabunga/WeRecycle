6a2c6227dc4c6a3584380dbb284684c4b952b2f1
francisnormanmacabunga <francisnormanmacabunga@gmail.com>
2018-11-01 20:14:52 +0800

    PD

--- resources/views/ProgramDirector/Transactions/editOrder.blade.php
+++ resources/views/ProgramDirector/Transactions/editOrder.blade.php
@@ -48,7 +48,7 @@
                                                                 <td>{{$order['status']}}</td>
                                                                 <td>
                                                                   <select class="select2 form-control custom-select" name="status" style="width: 100%; height:36px;">
-                                                                    <option>Choose status</option>
+                                                                    <optgroup label="{{$order['status']}}">
                                                                         <option value="Shipping">Shipping</option>
                                                                         <option value="Delivered">Delivered</option>
                                                                         <option value="Cancelled">Cancelled</option>
@@ -56,7 +56,7 @@
                                                                 </td>
                                                                 <td>
                                                                   <select class="select2 form-control custom-select" name="volunteer" style="width: 100%; height:36px;">
-                                                                    <option>Choose volunteer</option>
+                                                                    <optgroup label="{{$order->volunteer['firstname']}}">
                                                                     @foreach($volunteer as $volunteers)
                                                                       <option value="{{$volunteers->volunteerID}}">{{$volunteers->firstname}}</option>
                                                                     @endforeach
--- resources/views/ProgramDirector/Transactions/editRequest.blade.php
+++ resources/views/ProgramDirector/Transactions/editRequest.blade.php
@@ -1,76 +1,76 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
+
 <body>
-<div id="main-wrapper">
-  @include('navbar.pd-navbar')
-  <div class="page-wrapper">
-    <!-- ============================================================== -->
-    <!-- Bread crumb and right sidebar toggle -->
-    <!-- ============================================================== -->
-     <div class="page-breadcrumb">
-        <div class="row">
-            <div class="col-12 d-flex no-block align-items-center">
-                <div class="ml-auto text-right">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ url('/programdirector/requests') }}">Requests</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Edit</li>
-                        </ol>
-                    </nav>
+    <div id="main-wrapper">
+        @include('navbar.pd-navbar')
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-12 d-flex no-block align-items-center">
+                        <div class="ml-auto text-right">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="{{ url('/programdirector/requests') }}">Requests</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Edit</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-          {!! Form::open(['action' => ['ProgramDirector\RequestController@update', $request['transid']], 'method' => 'POST' ]) !!}
-                  <!-- Card -->
-                  <div class="card">
-                      <div class="card-body" style="height: 300px;">
-                        <h4 class="card-title">Update Transaction</h4>
-                                            <div class="card">
-                                                <div class="card-body">
-
-                                                  </div>
-                                                    <div class="table-responsive">
-                                                      <table class="table">
-                                                          <thead class="thead-light">
-                                                              <tr>
-                                                                  <th scope="col">Name</th>
-                                                                  <th scope="col">Status</th>
-                                                                  <th scope="col">Action</th>
-                                                                  <th scope="col">Volunteer</th>
-                                                              </tr>
-                                                          </thead>
-                                                          <tbody class="customtable">
-                                                              <tr>
-                                                                <td>{{$request->donor->firstname}} {{$request->donor->lastname}}</td>
-                                                                <td>{{$request['status']}}</td>
-                                                                <td>
-                                                                  <select class="select2 form-control custom-select" name="status" style="width: 100%; height:36px;">
-                                                                    <option value="" disabled selected>Choose status</option>
-                                                                        <option value="Shipping">Shipping</option>
-                                                                        <option value="Delivered">Delivered</option>
-                                                                        <option value="Cancelled">Cancelled</option>
-                                                                  </select>
-                                                                </td>
-                                                                <td>
-                                                                  <select class="select2 form-control custom-select" name="volunteer" style="width: 100%; height:36px;">
-                                                                    <option value="" disabled selected>Choose volunteer</option>
-                                                                    @foreach($volunteer as $volunteers)
-                                                                      <option value="{{$volunteers->volunteerID}}">{{$volunteers->firstname}}</option>
-                                                                    @endforeach
-                                                                  </select>
-                                                                </td>
-                                                              </tr>
-                                                          </tbody>
-                                                      </table>
-                                                      {{Form::hidden('_method','PUT')}}
-                                                      <input type="button" value="Save" class="btn btn-success btn-block btn-lg" data-toggle="modal" data-target="#Modal2" />
-                                                    </div>
-                                            </div>
-                      </div>
-                      <!-- Modal -->
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    {!! Form::open(['action' => ['ProgramDirector\RequestController@update', $request['transid']], 'method' => 'POST' ]) !!}
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-body" style="height: 300px;">
+                            <h4 class="card-title">Update Transaction</h4>
+                            <div class="card">
+                                <div class="card-body">
+                                </div>
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead class="thead-light">
+                                            <tr>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Status</th>
+                                                <th scope="col">Action</th>
+                                                <th scope="col">Volunteer</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="customtable">
+                                            <tr>
+                                                <td>{{$request->donor->firstname}} {{$request->donor->lastname}}</td>
+                                                <td>{{$request['status']}}</td>
+                                                <td>
+                                                    <select class="select2 form-control custom-select" name="status" style="width: 100%; height:36px;">
+                                                        <optgroup label="{{$request['status']}}">
+                                                            <option value="Shipping">Shipping</option>
+                                                            <option value="Delivered">Delivered</option>
+                                                            <option value="Cancelled">Cancelled</option>
+                                                    </select>
+                                                </td>
+                                                <td>
+                                                    <select class="select2 form-control custom-select" name="volunteer" style="width: 100%; height:36px;">
+                                                        <optgroup label="{{$request->volunteer['firstname']}}">
+                                                            @foreach($volunteer as $volunteers)
+                                                            <option value="{{$volunteers->volunteerID}}">{{$volunteers->firstname}}</option>
+                                                            @endforeach
+                                                    </select>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    {{Form::hidden('_method','PUT')}}
+                                    <input type="button" value="Save" class="btn btn-success btn-block btn-lg" data-toggle="modal" data-target="#Modal2" />
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Modal -->
                         <div class="modal fade" id="Modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
@@ -90,16 +90,16 @@
                                 </div>
                             </div>
                         </div>
-                  </div>
-            </form>
+                    </div>
+                    </form>
+                </div>
             </div>
-
+            <footer class="footer text-center">
+                Copyright &copy; 2018 WeRecycle
+            </footer>
+        </div>
     </div>
-    <footer class="footer text-center">
-      Copyright &copy; 2018 WeRecycle
-    </footer>
-  </div>
-</div>
-@include('navbar.footer')
-  </body>
-  </html>
+    @include('navbar.footer')
+</body>
+
+</html>

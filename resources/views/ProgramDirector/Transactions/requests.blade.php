cad71bfe8ed058f5835df5f5898dbb53fec5ff9b
jamespramono <jameskylejoko.pramono@benilde.edu.ph>
2018-11-04 22:54:24 +0800

    ui fix table pdf

--- resources/views/ProgramDirector/Transactions/requests.blade.php
+++ resources/views/ProgramDirector/Transactions/requests.blade.php
@@ -73,9 +73,9 @@
                                       </td>
                                       @else
                                       <td>
-                                      <a href="/programdirector/sendSMS-V-R/transactionID={{$requests->transid}}" data-toggle="tooltip" data-placement="top"  title="Message Volunteer"><i class="mdi mdi-message-reply-text"></i></a><br/>
-                                      <a href="/programdirector/sendSMS-D-R/transactionID={{$requests->transid}}" data-toggle="tooltip" data-placement="top"  title="Message Donor"><i class="mdi mdi-message-reply"></i></a><br/>
-                                      <a href="/programdirector/requests/{{$requests->transid}}/edit" data-toggle="tooltip" data-placement="top"  title="Edit"><i class="fas fas fa-edit"></i></a>
+                                      <a href="/programdirector/sendSMS-V-R/transactionID={{$requests->transid}}" data-toggle="tooltip" data-placement="top"  title="Message Volunteer"><button><i class="mdi mdi-message-reply-text"></i></button></a><br/>
+                                      <a href="/programdirector/sendSMS-D-R/transactionID={{$requests->transid}}" data-toggle="tooltip" data-placement="top"  title="Message Donor"><button><i class="mdi mdi-message-reply"></i></button></a><br/>
+                                      <a href="/programdirector/requests/{{$requests->transid}}/edit" data-toggle="tooltip" data-placement="top"  title="Edit"><button><i class="fas fas fa-edit"></button></i></a>
                                     </td>
                                       @endif
                                     </tr>

9e7c138e59bcdc413c7d4b1f91f362a86e2ec167
amielalcala <amielalcala05@gmail.com>
2018-11-06 00:43:32 +0800

    histor 1

--- resources/views/Donor/History/pointHistory.blade.php
+++ resources/views/Donor/History/pointHistory.blade.php
@@ -11,20 +11,11 @@
 	</section>
 
 	<!-- Shoping Cart -->
-	<div class="bg0 p-t-75 p-b-85">
 		<div class="container">
 
       <div class="flex-w flex-sb-m p-b-52">
-        <div class="flex-w flex-l-m filter-tope-group m-tb-10">
-          <a href="{{ url('/donor/pointhistory') }}" class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5">
-            All
-          </a>
-          <a href="{{ url('/donor/pointhistory/?status=Purchased') }}" class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5">
-            Purchased
-          </a>
-          <a href="{{ url('/donor/pointhistory/?status=Donated') }}" class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5">
-            Donated
-          </a>
+        <div class="flex-w flex-l-m filter-tope-group m-tb-10 align-right">
+
         </div>
       </div>
 
@@ -38,7 +29,7 @@
 								<tr class="table_head">
                   <th class="column-1"></th>
 									<th class="column-2">Activity</th>
-									<th class="column-3">Points Gained</th>
+									<th class="column-2">Points Gained</th>
 									<th class="text-center">Time Gained</th>
                   <th class="column-5"></th>
 								</tr>
@@ -47,7 +38,7 @@
                   <td class="text-center">
                   </td>
 									<td class="column-2">{{$points->activity}}</td>
-									<td class="column-3">
+									<td class="column-1">
                     {{$points->points}}
                   </td>
 									<td class="column-4">
@@ -72,35 +63,48 @@
 				</div>
 
 				<div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
-					<div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
+          <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
             <h4 class="mtext-112 cl2 p-b-33">
-              Categories
+              Sort by
             </h4>
 
             <ul>
               <li class="bor18">
-                <a href="{{url('/donor/transactionhistory')}}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
-                  Transaction History
-                </a>
-              </li>
-
-              <li class="bor18">
-                <a href="{{url('/donor/donationhistory')}}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
-                  Donation History
-                </a>
-              </li>
-
-              <li class="bor18">
                 <a href="{{ url('/donor/pointhistory') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
-                  Points History
+                  All
+                </a>
+              </li>
+
+              <li class="bor18">
+                <a href="{{ url('/donor/pointhistory/?status=Purchased') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
+                  Purchased
+                </a>
+              </li>
+
+              <li class="bor18">
+                <a href="{{ url('/donor/pointhistory/?status=Donated') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
+                  Donated
                 </a>
               </li>
             </ul>
-					</div>
+            <div class="p-t-50">
+            <h4 class="mtext-112 cl2 p-b-27">
+              History Tags
+            </h4>
+
+            <div class="flex-w m-r--5">
+              <a href="{{url('/donor/transactionhistory')}}" class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
+                Transaction
+              </a>
+              <a href="{{url('/donor/donationhistory')}}" class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
+                Donation
+              </a>
+            </div>
+          </div>
+          </div>
 				</div>
 			</div>
 		</div>
-	</div>
   @include('navbar.donor-footer')
   </body>
   </html>

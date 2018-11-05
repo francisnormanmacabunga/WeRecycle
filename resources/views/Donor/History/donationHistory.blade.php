ddb740cf0834b9c92826c76d6f6d25982b179d9e
amielalcala <amielalcala05@gmail.com>
2018-11-06 01:16:34 +0800

    donation WIP

--- resources/views/Donor/History/donationHistory.blade.php
+++ resources/views/Donor/History/donationHistory.blade.php
@@ -1,108 +1,136 @@
-@extends('layouts.frontend')
-@include('layouts.donor-nav')
-@section('content')
+<!DOCTYPE html>
+    <html lang="en">
+    @include('navbar.donor')
+    <body class="animsition">
 
-<div class="row">
-    <div class="col-lg-3">
-        <h3>Donation History</h3>
-        <div class="list-group">
-            <a href="{{url('/donor/donationhistory')}}" class="list-group-item">Donation History</a>
-            <a href="{{url('/donor/transactionhistory')}}" class="list-group-item">Transaction History</a>
-            <a href="{{ url('/donor/pointhistory') }}" class="list-group-item">Points History</a>
-        </div>
-        </div>
+    	<!-- Title page -->
+    	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url({{asset('donor-design/images/cart.jpg')}});">
+    		<h2 class="ltext-105 cl0 txt-center">
+    			Donation History
+    		</h2>
+    	</section>
 
-    <div class="col-lg-9">
+    	<!-- Shoping Cart -->
+    		<div class="container">
 
-      <a href="{{ url('/donor/donationhistory') }}">
-       <button style="float: right;">Reset</button>
-      </a>
+          <div class="flex-w flex-sb-m p-b-52">
+            <div class="flex-w flex-l-m filter-tope-group m-tb-10 align-right">
 
-      <a href="{{ url('/donor/donationhistory/?status=Cancelled') }}">
-        <button style="float: right;">Sort by Cancelled</button>
-      </a>
-
-      <a href="{{ url('/donor/donationhistory/?status=Delivered') }}">
-        <button style="float: right;">Sort by Delivered</button>
-      </a>
-
-      <a href="{{ url('/donor/donationhistory/?status=Shipping') }}">
-        <button style="float: right;">Sort by Shipping</button>
-      </a>
-
-      <a href="{{ url('/donor/donationhistory/?status=Processing') }}">
-        <button style="float: right;">Sort by Processing</button>
-      </a>
-
-      <br>
-      <br>
-
-        <div class="row">
-            @if(count($donation) > 0)
-            <table class="table table-bordered" class="fixed">
-              <thead>
-                  <tr>
-                  </tr>
-              </thead>
-              <tbody>
-                <tr>
-                    <th>Transaction ID</th>
-                    <th>Assigned Volunteer</th>
-                    <th>Product Name</th>
-                    <th>Quantity</th>
-                    <th>@sortablelink('created_at', 'Date')</th>
-                    <th>@sortablelink('status', 'Status')</th>
-                    <th>Action</th>
-                </tr>
-                @foreach ($donation as $donations)
-                @php
-                $cart = json_decode($donations->cart);
-                @endphp
-                @foreach($cart as $item)
-                <tr>
-                    <td>{{$donations->transid}}</td>
-                    <td>{{$donations->volunteer['firstname']}} {{$donations->volunteer['lastname']}}</td>
-                    <td>{{$item->name}}</td>
-                    <td>{{$item->qty}}</td>
-                    <td>{{date('F d, Y, h:i:sa', strtotime($donations->created_at))}}</td>
-                    <td>{{$donations->status}}</td>
-                    @if ($donations->status == 'Shipping' || $donations->status == 'Delivered' || $donations->status == 'Cancelled')
-                    <td>
-                        <form action="/cancel/{{$donations->transid}}">
-                            <input type="submit" value="Cancel" disabled />
-                        </form>
-                    </td>
-                    @else
-                    <td>
-                        <form action="/cancel/{{$donations->transid}}">
-                            <input type="submit" value="Cancel" onclick="Confirm()" />
-                        </form>
-                    </td>
-                    @endif
-                    <script type="text/javascript">
-                        function Confirm() {
-                            var confirm_value = document.createElement("INPUT");
-                            confirm_value.type = "hidden";
-                            confirm_value.name = "confirm_value";
-                            if (confirm("Do you want to save data?")) {
-                                confirm_value.value = "Yes";
-                            } else {
-                                confirm_value.value = "No";
-                            }
-                            document.forms[0].appendChild(confirm_value);
-                        }
-                    </script>
-                </tr>
-                @endforeach
-              @endforeach
-                </tbody>
-            </table>
-            @else
-            <div align="center" style="color:red;">
-                <h4 style="font-family:serif;">No donations found.</h4>
             </div>
-            @endif
-        </div>
-    </div>
+          </div>
 
-    @endsection
+    			<div class="row">
+    				<div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
+    					<div class="m-l-25 m-r--38 m-lr-0-xl">
+    						<div class="wrap-table-shopping-cart">
+                  @if(count($donation) > 0)
+    							<table class="table-shopping-cart">
+
+    								<tr class="table_head">
+                      <th>Transaction ID</th>
+                      <th>Assigned Volunteer</th>
+                      <th>Product Name</th>
+                      <th>Quantity</th>
+                      <th>@sortablelink('created_at', 'Date')</th>
+                      <th>@sortablelink('status', 'Status')</th>
+                      <th>Action</th>
+    								</tr>
+                    @foreach ($donation as $donations)
+                    @php
+                    $cart = json_decode($donations->cart);
+                    @endphp
+                    @foreach($cart as $item)
+    								<tr class="table_row">
+                      <td>{{$donations->transid}}</td>
+                      <td>{{$donations->volunteer['firstname']}} {{$donations->volunteer['lastname']}}</td>
+                      <td>{{$item->name}}</td>
+                      <td>{{$item->qty}}</td>
+                      <td>{{date('F d, Y, h:i:sa', strtotime($donations->created_at))}}</td>
+                      <td>{{$donations->status}}</td>
+                      @if ($donations->status == 'Shipping' || $donations->status == 'Delivered' || $donations->status == 'Cancelled')
+                      <td>
+                          <form action="/cancel/{{$donations->transid}}">
+                              <input type="submit" value="Cancel" disabled />
+                          </form>
+                      </td>
+                      @else
+                      <td>
+                          <form action="/cancel/{{$donations->transid}}">
+                              <input type="submit" value="Cancel"onclick="if(confirm('Are you sure?')) saveandsubmit(event);" />
+                          </form>
+                      </td>
+                      @endif
+    								</tr>
+    								@endforeach
+    								@endforeach
+                    @else
+    								<td colspan="10">
+    									<br />
+    									<div class="text-center">
+    										<h5 class="stext-110 cl2" style="color:red;">No records found.</h5>
+    									</div>
+    									<br />
+    								</td>
+    								@endif
+    							</table>
+    						</div>
+    					</div>
+    				</div>
+
+    				<div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
+              <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
+                <h4 class="mtext-112 cl2 p-b-33">
+                  Sort by
+                </h4>
+                <ul>
+                  <li class="bor18">
+                    <a href="{{ url('/donor/donationhistory') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
+                      All
+                    </a>
+                  </li>
+
+                  <li class="bor18">
+                    <a href="{{ url('/donor/donationhistory/?status=Cancelled') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
+                      Cancelled
+                    </a>
+                  </li>
+
+                  <li class="bor18">
+                    <a href="{{ url('/donor/pointhistory/?status=Delivered') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
+                      Delivered
+                    </a>
+                  </li>
+
+                  <li class="bor18">
+                    <a href="{{ url('/donor/pointhistory/?status=Shipping') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
+                      Shipping
+                    </a>
+                  </li>
+
+                  <li class="bor18">
+                    <a href="{{ url('/donor/pointhistory/?status=Processing') }}" class="dis-block stext-115 cl6 hov-cl1 trans-04 p-tb-8 p-lr-4">
+                      Processing
+                    </a>
+                  </li>
+                </ul>
+                <div class="p-t-50">
+                <h4 class="mtext-112 cl2 p-b-27">
+                  History Tags
+                </h4>
+
+                <div class="flex-w m-r--5">
+                  <a href="{{url('/donor/transactionhistory')}}" class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
+                    Transaction
+                  </a>
+                  <a href="{{ url('/donor/pointhistory') }}" class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
+                    Points
+                  </a>
+                </div>
+              </div>
+              </div>
+    				</div>
+    			</div>
+    		</div>
+      @include('navbar.donor-footer')
+      </body>
+      </html>

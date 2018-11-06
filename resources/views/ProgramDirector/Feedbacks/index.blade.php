776b5e9128a8952e02f3f68d95fa6869d96bbe0b
jamespramono <jameskylejoko.pramono@benilde.edu.ph>
2018-11-06 17:30:01 +0800

    feedback logo

--- resources/views/ProgramDirector/Feedbacks/index.blade.php
+++ resources/views/ProgramDirector/Feedbacks/index.blade.php
@@ -1,67 +1,9 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-  <html dir="ltr" lang="en">
-  <body>
-    <head>
-        <!-- Favicon icon -->
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
-    </head>
-  <div id="main-wrapper">
-    @include('navbar.pd-navbar')
-    <div class="page-wrapper">
-      <!-- ============================================================== -->
-      <!-- End Bread crumb and right sidebar toggle -->
-      <!-- ============================================================== -->
-      <!-- ============================================================== -->
-      <!-- Container fluid  -->
-      <!-- ============================================================== -->
-      <div class="container-fluid">
-          <div class="row">
-              <div class="col-12">
-                  <div class="card">
-                      <div class="card-body">
-                          <h5 class="card-title">Feedback</h5>
-                          <div class="table-responsive">
-                            @if(count($feedbacks) > 0)
-                              <table id="zero_config" class="table table-striped table-bordered">
-                                  <thead>
-                                      <tr>
-                                        <th>Username</th>
-                                        <th>Feedback</th>
-                                        <th>Rating</th>
-                                        <th>Date Applied</th>
-                                      </tr>
-                                  </thead>
-                                  <tbody>
-                                    @foreach ($feedbacks as $feedback)
-                                      <tr>
-                                        <td>{{$feedback->username}}</td>
-                                        <td>{{$feedback->feedback}}</td>
-                                        <td>{{$feedback->rating}}</td>
-                                        <td>{{date('F d, Y, h:i:sa', strtotime($feedback->created_at))}}</td>
-                                      </tr>
-                                      @endforeach
-                                  </tbody>
-                              </table>
-                              @else
-                              <div align="center" style="color:red;">
-                                <br>
-                                <br>
-                                <h5>No records found.</h5>
-                              </div>
-                              @endif
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-
-      <footer class="footer text-center">
-        Copyright &copy; 2018 WeRecycle
-      </footer>
-=======
 <html dir="ltr" lang="en">
+<head>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
+</head>
 
 <body>
     <div id="main-wrapper">
@@ -117,7 +59,6 @@
                 Copyright &copy; 2018 WeRecycle
             </footer>
         </div>
->>>>>>> cefc57a94839e0d7a3463d7dd82bd5631fc7ba09
     </div>
     @include('navbar.footer')
 </body>

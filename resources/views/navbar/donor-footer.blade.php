b2335187ea02864de58bc86e9f6ff347e3b62f9c
amielalcala <amielalcala05@gmail.com>
2018-11-16 00:54:45 +0800

    footer

--- resources/views/navbar/donor-footer.blade.php
+++ resources/views/navbar/donor-footer.blade.php
@@ -7,15 +7,84 @@
 
 <footer class="bg3 p-t-75 p-b-32" style="background-color: #1B4D3E">
 	<div class="container">
-		<div class="flex-c-m flex-w p-b-18">
-			<a href="{{url('/faqs')}}" class="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">
-				FAQS
+	<div class="row">
+		<div class="col-sm-6 col-lg-3 p-b-50">
+			<h4 class="stext-301 cl0 p-b-30">
+				Help
+			</h4>
+
+			<ul>
+				<li class="p-b-10">
+					<a href="{{ url('/donor/donors') }}" class="stext-107 cl7 hov-cl1 trans-04">
+						View Profile
+					</a>
+				</li>
+				<li class="p-b-10">
+					<a href="{{url('/faqs')}}" class="stext-107 cl7 hov-cl1 trans-04">
+						FAQs
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="col-sm-6 col-lg-3 p-b-50">
+			<h4 class="stext-301 cl0 p-b-30">
+				Points and Discounts
+			</h4>
+
+			<ul>
+				<li class="p-b-10">
+					<a href="{{ url('/donor/pointhistory') }}" class="stext-107 cl7 hov-cl1 trans-04">
+						Points History
+					</a>
+				</li>
+				<li class="p-b-10">
+					<a href="{{ url('/donor/discont') }}" class="stext-107 cl7 hov-cl1 trans-04">
+						Discount Codes
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="col-sm-6 col-lg-3 p-b-50">
+			<h4 class="stext-301 cl0 p-b-30">
+				History
+			</h4>
+			<ul>
+				<li class="p-b-10">
+					<a href="{{ url('/donor/transactionhistory') }}" class="stext-107 cl7 hov-cl1 trans-04">
+						Transaction History
+					</a>
+				</li>
+				<li class="p-b-10">
+					<a href="{{url('/donor/donationhistory')}}" class="stext-107 cl7 hov-cl1 trans-04">
+						Donation History
+					</a>
+				</li>
+
+			</ul>
+		</div>
+						<div class="col-sm-6 col-lg-3 p-b-50">
+							<h4 class="stext-301 cl0 p-b-30">
+		Greetings, {{ Auth::user()->username }}.
+							</h4>
+			<a href="{{ route('donor.logout') }}" onclick="event.preventDefault();
+										document.getElementById('logout-form').submit();"
+										class="flex-c-m stext-101 cl0 size-118 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer m-tb-5">
+				Logout
 			</a>
-</div>
+			<form id="logout-form" action="{{ route('donor.logout') }}" method="POST" style="display: none;">
+					@csrf
+			</form>
+	</div>
+	</div>
+
+	<div class="p-t-40">
 		<p class="stext-107 cl6 txt-center">
 			<font color="white"> Copyright &copy; 2018 WeRecycle™ </font>
 		</p>
 	</div>
+</div>
 </footer>
 <!--===============================================================================================-->
 	<script src="{{asset('donor-design/vendor/jquery/jquery-3.2.1.min.js')}}"></script>

0d486463b75694079c4ef25f6e7d9cde0e7385b0
amielalcala <amielalcala05@gmail.com>
2018-11-17 16:54:51 +0800

    back links

--- resources/views/ProgramDirector/Auth/passwords/email.blade.php
+++ resources/views/ProgramDirector/Auth/passwords/email.blade.php
@@ -48,7 +48,7 @@
                           <!-- pwd -->
                           <div class="row m-t-20 p-t-20">
                               <div class="col-12">
-                                  <a class="btn btn-danger btn-rounded" href="{{ url('/programdirector/login') }}" id="to-login" name="action">Back</a>
+                                  <a class="btn btn-danger btn-rounded" href="{{ url()->previous() }}" id="to-login" name="action">Back</a>
                                   <button class="btn btn-info float-right btn-rounded" type="submit" name="action">{{ __('Recover') }}</button>
                               </div>
                           </div>
--- resources/views/ProgramDirector/Transactions/sendSMS-V-R.blade.php
+++ resources/views/ProgramDirector/Transactions/sendSMS-V-R.blade.php
@@ -16,7 +16,7 @@
             <div class="col-12 d-flex no-block align-items-center">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ url('/programdirector/requests') }}">Requests</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url()->previous() }}">Requests</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Message Volunteer</li>
                         </ol>
                     </nav>

170088d4ae7adb2e6d8c83b51c1886a95b1b4387
francisnormanmacabunga <francisnormanmacabunga@gmail.com>
2018-11-20 14:51:18 +0800

    Final Update

--- resources/views/pages/index2.blade.php
+++ resources/views/pages/index2.blade.php
@@ -85,7 +85,7 @@
               <div class="col-md-10 col-md-offset-1">
                 <div class="probootstrap-slider-text text-center">
                   <h1 class="probootstrap-heading probootstrap-animate mb20">Shop Organic Fertilizers</h1>
-                  <p class="probootstrap-animate"><a href="{{ url('/shop') }}" class="btn btn-ghost btn-ghost-white">Shop</a></p>
+                  <p class="probootstrap-animate"><a href="{{ url('/shop') }}" class="btn btn-ghost btn-ghost-white">Catalog</a></p>
                 </div>
               </div>
             </div>
